be2ef1d8a1f4ff897f57f478d611938b2cc6bc6e
Ahmed Liqaa <ahmadliqaa@gmail.com>
2020-11-01 00:22:18 +0300

    from windows

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -7,6 +7,11 @@ import 'core-js';
 import 'regenerator-runtime/runtime';
 import $ from 'jquery';
 import {
+  gsap,
+  TimelineMax,
+  TweenMax
+} from 'gsap'
+import {
   Link
 } from '../_modules/link/link';
 
@@ -18,15 +23,71 @@ import {
 
 (function (_) {
   $(document).ready(function () {
-    const toggleMenu = _('.nav')
+    const toggleBtnMenu = _('.nav')
     const mainMenu = _('.menu')
     const btnSocial = _('.main-social')
     const hoverLinks = _('.js-hover-links')
+    const menuItem = _('.js-menu-item')
+    const infoItems = _('.js-infos-item')
 
-    toggleMenu.on('click', function () {
-      _(this).toggleClass('is-active')
-      mainMenu.toggleClass('is-active')
-    })
+
+    console.log(menuItem);
+
+    function toggleMenu() {
+      mainMenu.toggleClass('is-active'),
+
+        //FOR OVERLAY FIXED DIV
+        setTimeout(() => {
+          toggleBtnMenu.toggleClass('is-active');
+        }, 1e3);
+
+
+    }
+
+    function closeMenu() {
+      mainMenu.removeClass('is-active');
+      toggleBtnMenu.removeClass('is-active');
+    }
+
+    function menuItemFade() {
+      var itemTransition = new TimelineMax();
+
+      itemTransition.staggerFromTo(menuItem, 1.2, {
+        opacity: 0,
+        xPercent: -20
+      }, {
+        opacity: 1,
+        xPercent: 0,
+        ease: "power3.out"
+      }, .1, .5)
+    }
+
+
+    function infoItemsFade() {
+      var infoItemstl = new TimelineMax();
+
+      infoItemstl.staggerFromTo(infoItems, 1.2, {
+        opacity: 0
+      }, {
+        opacity: 1,
+        ease: "power3.out"
+      }, .2, 1)
+    }
+
+
+    function init() {
+
+      toggleBtnMenu.on('click', function () {
+        toggleMenu(),
+          mainMenu.hasClass('is-active') && ((menuItemFade()), (infoItemsFade()))
+      })
+    }
+    init();
+
+
+
+
+
 
     btnSocial.on('click', function () {
       _(this).toggleClass('is-open');
@@ -41,6 +102,7 @@ import {
     }, function () {
       hoverLinks.removeClass('is-not-hover is-hover')
     });
+
 
   });
 })($);
